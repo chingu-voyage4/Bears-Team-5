ee981eaa5640cbbe71f5570e924e87156ed12b43
JethroF22 <fredericks22.jethro@gmail.com>
2018-03-01 19:08:48 +0200

    Configured environment for React development

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// entry point -> output file
+
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "public")
+  }
+};
