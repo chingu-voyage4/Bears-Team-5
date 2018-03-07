09b4b4488c8dbec3ff1754978f1762de0e07c63a
JethroF22 <fredericks22.jethro@gmail.com>
2018-03-07 13:38:23 +0200

    Create PageHeader component

---
+++ src/components/PageHeader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageHeader = () => (
+  <div>
+    <img src="" alt="logo goes here" />
+    <button>Sign in</button>
+    <button>Create account</button>
+  </div>
+);
+
+export default PageHeader;
