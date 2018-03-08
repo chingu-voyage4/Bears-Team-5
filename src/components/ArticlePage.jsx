350230ad82e1346c1d09c496492ebfc8e48f7078
JethroF22 <fredericks22.jethro@gmail.com>
2018-03-08 15:45:05 +0200

    Add a like button to ArticlePage component

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -9,9 +9,15 @@ export default class ArticlePage extends React.Component {
           imgUrl="https://pixabay.com/get/ea37b1082bf0033ed1584d05fb0938c9bd22ffd41cb3114494f4c67aa1/cock-3207342_1280.png"
           authorName="Colonel Cockerel"
           authorDetails="Leader of the Chicken Uprising, sworn enemy of KFC"
-          date="Mar 8"
-          readingTime="20 sec"
         />
+        <AuthorDetails
+          imgUrl="https://pixabay.com/get/ea37b1082bf0033ed1584d05fb0938c9bd22ffd41cb3114494f4c67aa1/cock-3207342_1280.png"
+          authorName="Colonel Cockerel"
+          authorDetails="Leader of the Chicken Uprising, sworn enemy of KFC"
+        />
+        <div>
+          <button>Like</button>
+        </div>
       </div>
     );
   }
