a95d26222c52152bd6cec640cd9e2248e0ab4c25
JethroF22 <fredericks22.jethro@gmail.com>
2018-03-09 15:43:43 +0200

    Import Comment in ArticlePage and render article comments

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,7 +1,16 @@
-import React from "react";
-import AuthorDetails from "./AuthorDetails";
+import React from 'react';
+import AuthorDetails from './AuthorDetails';
+import Comments from './Comments';
 
 export default class ArticlePage extends React.Component {
+  state = {
+    article: {
+      title: this.props.article.title,
+      body: this.props.article.body,
+    },
+    comments: this.props.comments,
+  };
+
   render() {
     return (
       <div>
@@ -10,6 +19,8 @@ export default class ArticlePage extends React.Component {
           authorName="Colonel Cockerel"
           authorDetails="Leader of the Chicken Uprising, sworn enemy of KFC"
         />
+        <h2>{this.state.article.title}</h2>
+        <p>{this.state.article.body}</p>
         <AuthorDetails
           imgUrl="https://i.imgur.com/hyqmyzn.png"
           authorName="Colonel Cockerel"
@@ -29,7 +40,27 @@ export default class ArticlePage extends React.Component {
             <a href="#">Suggested Article</a>
           </li>
         </ul>
+        <h3>Comments go here</h3>
+        <Comments comments={this.state.comments} />
       </div>
     );
   }
 }
+
+ArticlePage.defaultProps = {
+  article: {
+    title: 'Article Title',
+    body:
+      'Lorem ipsum dolor sit amet consectetur adipiscing elit faucibus sociosqu, nostra habitant at nam integer primis auctor non, feugiat risus sodales odio posuere augue ad aliquet.',
+  },
+  comments: [
+    {
+      userName: 'Jethro',
+      commentBody: 'This is my comment',
+    },
+    {
+      userName: 'Lisa',
+      commentBody: 'React is amazing',
+    },
+  ],
+};
--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Comment = props => (
   <div>
     <img src="" alt="User logo goes here" />
-    <p>{props.username}</p>
+    <p>{props.userName}</p>
     <p>{props.commentBody}</p>
     <button>Like</button>
     <button>Bookmark</button>
