7f22c7045f3ee5b0a5a0adb30b8d0a1fa623dada
JethroF22 <fredericks22.jethro@gmail.com>
2018-03-10 13:55:30 +0200

    Change prop names, add default value for details prop

--- src/components/AuthorDetails.jsx
+++ src/components/AuthorDetails.jsx
@@ -3,16 +3,20 @@ import PropTypes from 'prop-types';
 
 const AuthorDetails = props => (
   <div>
-    <img src={props.imgUrl} height="50" width="50" />
-    <p>{props.authorName}</p>
-    <p>{props.authorDetails}</p>
+    <img src={props.imgUrl} height="50" width="50" alt="author icon" />
+    <p>{props.name}</p>
+    <p>{props.details === '' ? 'Draft' : props.details}</p>
   </div>
 );
 
 AuthorDetails.propTypes = {
   imgUrl: PropTypes.string.isRequired,
-  authorName: PropTypes.string.isRequired,
-  authorDetails: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  details: PropTypes.string,
+};
+
+AuthorDetails.defaultProps = {
+  details: '',
 };
 
 export default AuthorDetails;
