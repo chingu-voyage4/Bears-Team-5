3a94634981ba24275d6b20f7186446611863aa74
JethroF22 <fredericks22.jethro@gmail.com>
2018-03-10 14:02:37 +0200

    Change prop names in AuthorDetails
    authorDetails => details, authorname => name

--- src/components/AuthorDetails.jsx
+++ src/components/AuthorDetails.jsx
@@ -4,19 +4,19 @@ import PropTypes from 'prop-types';
 const AuthorDetails = props => (
   <div>
     <img src={props.imgUrl} height="50" width="50" alt="user icon" />
-    <p>{props.authorName}</p>
-    <p>{props.authorDetails ? 'Draft' : props.authorDetails}</p>
+    <p>{props.name}</p>
+    <p>{props.details ? 'Draft' : props.details}</p>
   </div>
 );
 
 AuthorDetails.propTypes = {
   imgUrl: PropTypes.string.isRequired,
-  authorName: PropTypes.string.isRequired,
-  authorDetails: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  details: PropTypes.string,
 };
 
 AuthorDetails.defaultProps = {
-  authorDetails: '',
+  details: '',
 };
 
 export default AuthorDetails;
