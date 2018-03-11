5d428c1f8009707d135aa74e924249fdb168e22d
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-03-11 19:58:52 +0200

    establish connection to mySQL database
    required connection info is provided by the environment variables

--- models/db.js
+++ models/db.js
@@ -8,15 +8,16 @@ const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
-})
+  database: process.env.DB_NAME,
+  multipleStatements: true
+});
 
 connection.connect(function (err) {
   if (err) {
-    console.error('error connecting: ' + err.stack);
+    console.error(`error connecting: ${err.stack}`);
     return;
   }
-  console.log('connected as id ' + connection.threadId);
+  console.log(`connected as id: ${connection.threadId}`);
 });
 
 module.exports = connection;
