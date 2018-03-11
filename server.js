4b2431e040552f80738fa11c00953d0244c14672
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-03-11 20:07:23 +0200

    add user route handler

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const routes = require('./routes/api');
+const users = require('./routes/user');
 const bodyParser = require('body-parser');
+
+if (process.env.NODE_ENV === 'production') { console.log = function () { }; }
 
 const app = express();
 
-/*if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').load();
-}*/
-
 app.use(bodyParser.json());
-app.use('/api', routes);
+app.use('/api', users);
 
 app.listen(process.env.PORT || 4000, function () {
   console.log('listening for requests');
-})
+});
+
+module.exports = app;
