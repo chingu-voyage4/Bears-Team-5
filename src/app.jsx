9b67688ca42a690286410de4b7f1d96a5e3849a0
JethroF22 <fredericks22.jethro@gmail.com>
2018-03-14 15:42:35 +0200

    Revert "Add default values for `likedArticles` and `publishedArticles` props"
    This reverts commit 1e2749fb6a23e289b6eb4fd630213bf367c349cf.

--- src/app.jsx
+++ src/app.jsx
@@ -1,28 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ProfilePage from './components/ProfilePage';
+import CreateArticle from './components/CreateArticle';
 
-const articles = [
-  {
-    title: 'Do or Do Not, There Is No Try',
-    author: 'Yoda'
-  },
-  {
-    title: 'I Am Your Father!',
-    author: 'Darth Vader'
-  },
-  {
-    title: 'May the 4th Be With You',
-    author: 'George Lucas'
-  }
-];
-
-ReactDOM.render(
-  <ProfilePage
-    numFollowers={11}
-    numFollowing={1}
-    username="Jethro William Fredericks"
-    likedArticles={articles}
-  />,
-  document.getElementById('app')
-);
+ReactDOM.render(<CreateArticle />, document.getElementById('app'));
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,35 +1,11 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import ArticleList from './ArticleList';
+import React, { Component } from 'react';
 
 const ProfilePage = props => (
   <div>
-    <h2>
-      <strong>{props.username}</strong>
-    </h2>
-    <p>{props.numFollowing} following</p>
-    <p>{props.numFollowers} followers</p>
+    <h2>{props.username}</h2>
+    <p>{props.numFollowing}</p>
+    <p>{props.numFollowers}</p>
     <img src={props.userImgURL} height="30px" width="30px" />
-    <ArticleList heading="Liked Articles" />
-    <ArticleList heading="Published Articles" />
-    <div>
-      Icons made by{' '}
-      <a href="http://www.freepik.com" title="Freepik">
-        Freepik
-      </a>{' '}
-      from{' '}
-      <a href="https://www.flaticon.com/" title="Flaticon">
-        www.flaticon.com
-      </a>{' '}
-      is licensed by{' '}
-      <a
-        href="http://creativecommons.org/licenses/by/3.0/"
-        title="Creative Commons BY 3.0"
-        target="_blank"
-      >
-        CC 3.0 BY
-      </a>
-    </div>
   </div>
 );
 
@@ -37,15 +13,11 @@ ProfilePage.propTypes = {
   username: PropTypes.string.isRequired,
   numFollowing: PropTypes.number.isRequired,
   numFollowers: PropTypes.number.isRequired,
-  likedArticles: PropTypes.array,
-  publishedArticles: PropTypes.array,
   userImgURL: PropTypes.string
 };
 
 ProfilePage.defaultProps = {
-  userImgURL: 'https://i.imgur.com/mECBxga.png',
-  likedArticles: [],
-  publishedArticles: []
+  userImgURL: 'https://i.imgur.com/mECBxga.png'
 };
 
 export default ProfilePage;
