b4043fe67fa7b00d351eb5b6a2b09f95ee8e002a
JethroF22 <fredericks22.jethro@gmail.com>
2018-03-20 18:29:52 +0200

    Wire up onChange handlers for each input

--- src/components/SignUpPage.jsx
+++ src/components/SignUpPage.jsx
@@ -8,28 +8,76 @@ export default class SignUpPage extends Component {
     confirmPassword: ''
   };
 
+  onUsernameChange = e => {
+    const value = e.target.value;
+    this.setState(() => ({
+      username: value
+    }));
+  };
+
+  onEmailChange = e => {
+    const value = e.target.value;
+    this.setState(() => ({
+      email: value
+    }));
+  };
+
+  onPasswordChange = e => {
+    const value = e.target.value;
+    this.setState(() => ({
+      password: value
+    }));
+  };
+
+  onConfirmPasswordChange = e => {
+    const value = e.target.value;
+    this.setState(() => ({
+      confirmPassword: value
+    }));
+  };
+
   render() {
     return (
       <div>
-        <form contentType="application/json">
+        <form>
           <label htmlFor="username">
             Username:
-            <input type="text" name="username" />
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.onUsernameChange}
+            />
           </label>
           <br />
           <label htmlFor="email">
             Email:
-            <input type="email" name="email" />
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.onEmailChange}
+            />
           </label>
           <br />
           <label htmlFor="password">
             Password:
-            <input type="password" name="password" />
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.onPasswordChange}
+            />
           </label>
           <br />
           <label htmlFor="confirmPassword">
             Confirm Password:
-            <input type="password" name="confirmPassword" />
+            <input
+              type="password"
+              name="confirmPassword"
+              value={this.state.confirmPassword}
+              onChange={this.onConfirmPasswordChange}
+            />
           </label>
         </form>
       </div>
