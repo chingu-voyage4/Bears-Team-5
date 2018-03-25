dfcd5657e88f58353cf5cce823bfe9c37a33c05b
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-03-25 16:08:11 +0200

    allow CORS with pre-flights

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const users = require('./routes/user');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 if (process.env.NODE_ENV === 'production') { console.log = function () { }; }
 
 const app = express();
 
 app.use(bodyParser.json());
+app.options('*', cors());
 app.use('/api', users);
 
 app.listen(process.env.PORT || 4000, function () {
