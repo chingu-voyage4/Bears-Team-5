0b9162be07d7ac1169de137d5e2fe2b1a36b961e
JethroF22 <fredericks22.jethro@gmail.com>
2018-03-27 12:56:02 +0200

    Install redux and redux-thunk

--- src/app.jsx
+++ src/app.jsx
@@ -2,21 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import UserAuthentication from './components/UserAuthentication';
 
-// const likedArticles = [
-//   {
-//     author: {
-//       username: 'Yoda',
-//       details: 'Awesome, I am'
-//     },
-//     title: 'Do or do not, there is not try'
-//   },
-//   {
-//     author: {
-//       username: 'Darth Vader',
-//       details: 'You have failed me yet again'
-//     },
-//     title: 'I am your father!'
-//   }
-// ];
-
 ReactDOM.render(<UserAuthentication />, document.getElementById('app'));
