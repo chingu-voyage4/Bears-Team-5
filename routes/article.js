9d41853eebc8764b57e976e5dc48a90f9b096cc8
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-04-12 02:50:56 +0200

    implement get single article handler

--- routes/article.js
+++ routes/article.js
@@ -11,6 +11,57 @@ const moment = require('moment');
 const router = express.Router();
 
 
+
+router.get('/articles/:slug', [function (req, res, next) {
+  db.getConnection(function (err, connection) {
+    if (err) {
+      res.status(500).json({ msg: 'internal server error' });
+      return;
+    }
+    connection.query('SELECT `article_id`, `title`, `body`, `category`, `date`, `likes`, `image` FROM `article` WHERE `slug` = ?',
+      [req.params.slug], function (err, results) {
+        if (err) {
+          res.status(500).json({ msg: 'internal server error' });
+          connection.release();
+          return;
+        }
+        if (results.length === 0) {
+          connection.release();
+          res.status(404).json({ msg: 'article not found' });
+          return;
+        }
+        connection.release();
+        results[0].date = moment.parseZone(results[0].date).format('YYYY-MM-DD');
+        req.results = results[0];
+        next();
+      });
+  });
+}, optionalAuth, function (req, res) {
+  if (req.authData) {
+    db.getConnection(function (err, connection) {
+      if (err) {
+        res.status(500).json({ msg: 'internal server error' });
+        return;
+      }
+      connection.query('SELECT `user_id` FROM `article` WHERE `article_id` = ?', [req.results.article_id], function (err, results) {
+        if (err) {
+          res.status(500).json({ msg: 'internal server error' });
+          connection.release();
+          return;
+        }
+        connection.release();
+        if (results[0].user_id === req.authData.id) {
+          req.results.personal = true;
+          return res.status(200).json({ article: req.results });
+        }
+      });
+    });
+  } else {
+    req.results.personal = false;
+    return res.status(200).json({ article: req.results });
+  }
+}])
+
 router.post('/articles', [auth,
   body('title', 'title must not be empty and can not be more than 50 characters long').exists().isLength({ min: 1, max: 50 }),
   body('body', 'body can not be empty').exists().isLength({ min: 1 }),
