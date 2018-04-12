eb91f3e928c83a698dec55d06b0212e95edc980e
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-04-12 02:51:24 +0200

    implement tests for get single article handler

--- tests/article.test.js
+++ tests/article.test.js
@@ -22,7 +22,22 @@ function seed() {
         }
       );
     });
+  });
+};
 
+function articleSeed(id) {
+  return new Promise(function (resolve) {
+    db.query(
+      `SET FOREIGN_KEY_CHECKS = 0;
+        DELETE FROM \`article\`;
+        INSERT INTO \`article\` (user_id, title, body, category, date, slug) VALUES (?, ?, ?, ?, ?, ?);
+        INSERT INTO \`article\` (user_id, title, body, category, date, slug) VALUES (?, ?, ?, ?, ?, ?);
+        SET FOREIGN_KEY_CHECKS = 1;`,
+      [id, 'programming lessons and tips', 'abcdefg', 'technology', '2014-04-16', 'programming-lessons-and-tips-ab18981', id, 'dance moves', 'lorem ipsum', 'culture', '2017-05-22', 'dance moves-e51687c'], function (err) {
+        if (err) throw err;
+        resolve();
+      }
+    );
   });
 };
 
@@ -257,5 +272,79 @@ describe('post article handler', function () {
         done();
       });
   });
+});
+
+
+describe('get single article handler', function () {
+  var userId;
+  var token;
+  before(function (done) {
+    seed().then(getUserId).then(function (response) {
+      return new Promise(function (resolve) {
+        userId = response;
+        token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+          expiresIn: "3h"
+        });
+        resolve(userId);
+      });
+    }).then((response) => articleSeed(response)).then(done);
+  });
+
+  it('responds with 200 and body contains personal:false if no JWT is included', function (done) {
+    request(app).get(`/api/articles/dance moves-e51687c`)
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        if (err) done(err);
+        expect(res.body).to.have.property('article');
+        expect(res.body.article.length === 8, 'slug string length is equal to 8, it includes all article info');
+        done();
+      });
+  });
+
+  it('responds with 200 and body contains personal:false if the JWT of someone else is included', function (done) {
+    request(app).get(`/api/articles/dance moves-e51687c`)
+      .set({
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiaWF0IjoxNTIxMTk4MzUzLCJleHAiOjE1MjEyMDkxNTN9.ue9Ef8qBH4kQmnbngoWvjUm9fQhuvHV9l_BoHP2uVoc'
+      })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        if (err) done(err);
+        expect(res.body).to.have.property('article');
+        expect(res.body.article.length === 8, 'slug string length is equal to 8, it includes all article info');
+        expect(res.body.article.personal).to.be.false;
+        done();
+      });
+  });
+
+  it('responds with 200 and body contains personal:true if the JWT of the owner is included', function (done) {
+    request(app).get(`/api/articles/dance moves-e51687c`)
+      .set({
+        Authorization: `Bearer ${token}`
+      })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        if (err) done(err);
+        expect(res.body).to.have.property('article');
+        expect(res.body.article.length === 8, 'slug string length is equal to 8, it includes all article info');
+        expect(res.body.article.personal).to.be.true;
+        done();
+      });
+  });
+
+  it('responds with 404 if article doesnt not exist, and body contains error', function (done) {
+    request(app).get(`/api/articles/economics-101-e13q9we`)
+      .expect(404)
+      .expect('Content-Type', /json/)
+      .end(function (err, res) {
+        if (err) done(err);
+        expect(res.body).to.have.property('msg');
+        expect(res.body.msg).to.equal('article not found');
+        done();
+      });
+  });
+
 
 });
