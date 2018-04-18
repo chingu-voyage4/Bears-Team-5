18f3bc9baebe3d14af3094cf55bf0865c28f2218
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-04-18 20:39:32 +0200

    Remove case for SET_ERROR from articles reducer

--- src/reducers/articles.js
+++ src/reducers/articles.js
@@ -2,10 +2,8 @@ const articlesReducerDefaultState = [];
 
 export default (state = articlesReducerDefaultState, action) => {
   switch (action.type) {
-    case "CREATE_ARTICLE":
+    case 'CREATE_ARTICLE':
       return [...state, action.article];
-    case 'SET_ERROR':
-      return { ...state, error: action.error };
     default:
       return state;
   }
