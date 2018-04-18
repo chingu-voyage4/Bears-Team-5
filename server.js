d67261e9b3322732c0ff6494b0027be5edae90bc
Ahmed Al-Obaidi <destiny_hacker@hotmail.com>
2018-04-18 05:52:05 +0200

    implement MVPs get articles feed handler
    still missing getting featured and personal articles

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const users = require('./routes/user');
 const articles = require('./routes/article');
 const likes = require('./routes/like');
-const follows = require('./routes/follow')
+const follows = require('./routes/follow');
+const feeds = require('./routes/feed');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -12,7 +13,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.options('*', cors());
-app.use('/api', users, articles, likes, follows);
+app.use('/api', users, articles, likes, follows, feeds);
 
 app.listen(process.env.PORT || 4000, function () {
   console.log('listening for requests');
