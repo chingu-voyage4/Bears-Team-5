fee118e211e33b9b754af278b926d86253e200e5
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-04-19 00:17:44 +0200

    Change structure of articles reducer state from array to object

--- src/reducers/articles.js
+++ src/reducers/articles.js
@@ -1,11 +1,11 @@
-const articlesReducerDefaultState = [];
+const articlesReducerDefaultState = {};
 
 export default (state = articlesReducerDefaultState, action) => {
   switch (action.type) {
     case 'CREATE_ARTICLE':
-      return [...state, action.article];
+      return { ...state, feed: [...state.feed, action.article] };
     case 'SET_ARTICLES':
-      return action.articles;
+      return { feed: action.articles };
     default:
       return state;
   }
