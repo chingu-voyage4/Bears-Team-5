4f8587d14efcd947efa7cc427f2b62f0f00e8216
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-04-18 18:22:09 +0200

    Create actions for setting login errors and publishing errors

---
+++ src/actions/errors.js
@@ -0,0 +1,10 @@
+export const setLoginErrors = errors => ({
+  type: 'SET_LOGIN_ERRORS',
+  errors
+});
+
+export const setPublishingError = error => ({
+  type: 'SET_ARTICLE_PUBLISHING_ERROR',
+  error
+});
+
