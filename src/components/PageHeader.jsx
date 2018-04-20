5a0be08124a45cc49022b318a2b577877c4ebdc1
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-04-20 22:32:40 +0200

    Change PageHeader to class-based component

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import UserAuthentication from './UserAuthentication';
 
 const token = localStorage.getItem('token') || '';
 const username = localStorage.getItem('username');
 
-const PageHeader = () => (
-  <div className="header">
-    <h3 className="header__title">Medium Clone</h3>
-    {token === '' ? (
-      <UserAuthentication />
-    ) : (
-      <Link to={`/profile/${username}`} >
-          Welcome, {username}!
-          <button>Log Out</button>
-        </Link>
-      )}
-  </div>
-);
+class PageHeader extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h3 className="header__title">Medium Clone</h3>
+        {token === '' ? (
+          <UserAuthentication />
+        ) : (
+          <Link to={`/profile/${username}`} >
+              Welcome, {username}!
+              <button>Log Out</button>
+            </Link>
+          )}
+      </div>
+    );
+  }
+}
 
 export default PageHeader;
