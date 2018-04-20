ca93f604646a8fb04d748e59ce8b21beb42e0ce8
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-04-20 14:04:32 +0200

    Wrap each ArticleListItem in a Link

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import ArticleListItem from './ArticleListItem';
 
 const ArticleList = props => (
@@ -10,15 +11,17 @@ const ArticleList = props => (
       ? `You have not ${props.type === 'Liked Articles' ? 'liked' : 'published'} any articles`
       : props.articles.map((article) => {
         return (
-          <ArticleListItem
-            articleTitle={article.title}
-            articleAuthor={props.username}
-            articleImg={article.image}
-            key={Math.floor(Math.random() * 99999)}
-          />
+          <Link to={`/articles/edit/${article.article_id}`}>
+            <ArticleListItem
+              articleTitle={article.title}
+              articleAuthor={props.username}
+              articleImg={article.image}
+              key={Math.floor(Math.random() * 99999)}
+            />
+          </Link>
         );
       })}
-  </div>
+  </div >
 );
 
 ArticleList.propTypes = {
