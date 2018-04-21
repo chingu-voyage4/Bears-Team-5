8fd1ed2d867c5fb39090c8bbdf4f07323700f8c1
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-04-21 04:21:09 +0200

    Change PageHeader to class-based component with state + onClick handler

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,31 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { setMessage } from '../actions/messages';
 import UserAuthentication from './UserAuthentication';
 
-const token = localStorage.getItem('token') || '';
-const username = localStorage.getItem('username');
+class PageHeader extends Component {
+  state = {
+    token: localStorage.getItem('token') || '',
+    username: localStorage.getItem('username')
+  }
 
-const PageHeader = props => (
-  <div className="header">
-    {props.message && <p>{props.messge}</p>}
-    <Link to="/">
-      <h3 className="header__title">Medium Clone</h3>
-    </Link>
-    {token === '' ? (
-      <UserAuthentication />
-    ) : (
-      <Link to={`/profile/${username}`} >
-          Welcome, {username}!
-          <br />
-          <button>Log Out</button>
+  onClick = (e) => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('username');
+    this.props.setMessage("Logged out successfully");
+    this.props.history.push("/");
+  }
+
+  render() {
+    return (
+      <div className="header">
+        {this.props.message && <p>{this.props.message}</p>}
+        <Link to="/">
+          <h3 className="header__title">Medium Clone</h3>
         </Link>
-      )}
-  </div>
-);
+        {this.state.token === '' ? (
+          <UserAuthentication  />
+        ) : (
+            <Link to={`/profile/${this.state.username}`} >
+              Welcome, {this.state.username}!
+              <br />
+              <button onClick={this.onClick}>Log Out</button>
+            </Link>
+          )}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   message: state.messages.message
 });
 
-export default connect(mapStateToProps)(PageHeader);
+const mapDispatchToProps = dispatch => ({
+  setMessage: (message) => dispatch(setMessage(message))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PageHeader);
