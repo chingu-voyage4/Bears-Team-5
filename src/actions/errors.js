03f8be4d934f6be6e2cc96c5b61f8246a5c2be85
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-04-21 05:28:02 +0200

    Create action for setSignUpErrors

--- src/actions/errors.js
+++ src/actions/errors.js
@@ -3,6 +3,11 @@ export const setLoginErrors = loginError => ({
   loginError
 });
 
+export const setSignUpErrors = signUpErrors => ({
+  type: 'SET_SIGN_UP_ERRORS',
+  signUpErrors
+});
+
 export const setPublishingError = publishingError => ({
   type: 'SET_ARTICLE_PUBLISHING_ERROR',
   publishingError
