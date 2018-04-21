5a1839ff3ad4fe299a108201ba464c4b09f2da4d
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-04-21 04:47:36 +0200

    Create startSignUp action

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -23,12 +23,35 @@ export const startLogIn = (userCredentials) => {
         dispatch(clearError('loginError'));
         localStorage.setItem('token', response.data.token);
         localStorage.setItem('username', userCredentials.username);
-        dispatch(setMessage('Log In Successful'));
-        return dispatch(logIn(userCredentials.username));
+        return dispatch(setMessage('Log In Successful'));
       })
       .catch((error) => {
         const errorMsg = error.response.data.msg;
         dispatch(setLoginErrors(errorMsg));
+      });
+  };
+};
+
+export const startSignUp = (userCredentials) => {
+  return (dispatch) => {
+    const url = 'http://localhost:4000/api/register';
+    const config = {
+      url,
+      method: 'post',
+      data: JSON.stringify(userCredentials),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    return axios(config)
+      .then((response) => {
+        // dispatch(clearError('loginError'));
+        return dispatch(setMessage('Sign up Successful'));
+      })
+      .catch((error) => {
+        const errors = error.response.data.errors;
+        console.log(errors);
+        // dispatch(setLoginErrors(errorMsg));
       });
   };
 };
