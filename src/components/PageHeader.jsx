e772056578c378e5a18557b95fb84b7c98889286
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-04-21 04:42:04 +0200

    Remove UserAuthentication and render all it's content in PageHeader

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,21 +1,51 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import Modal from 'react-modal';
 import { setMessage } from '../actions/messages';
 import UserAuthentication from './UserAuthentication';
+import SignUpForm from './SignUpForm';
+import LogInForm from './LogInForm';
+import { startLogIn } from '../actions/auth';
+
 
 class PageHeader extends Component {
   state = {
-    token: localStorage.getItem('token') || '',
-    username: localStorage.getItem('username')
-  }
+    token: '',
+    username: '',
+    modalContent: 'Sign Up',
+    modalIsOpen: false
+  };
 
   onClick = (e) => {
     localStorage.removeItem('token');
     localStorage.removeItem('username');
+    this.setState(() => ({
+      token: '',
+      username: ''
+    }))
     this.props.setMessage("Logged out successfully");
-    this.props.history.push("/");
   }
+
+  triggerModal = e => {
+    const modalContent = e.target.name;
+    this.setState(() => ({ modalContent, modalIsOpen: true }));
+  };
+
+  closeModal = () => {
+    this.setState(() => ({ modalIsOpen: false }));
+  };
+
+  onSubmit = userCredentials => {
+    this.props.startLogIn(userCredentials)
+      .then(() => {
+        this.setState(() => ({
+          token: localStorage.getItem('token'),
+          username: localStorage.getItem('username')
+        }))
+        this.closeModal();
+      });
+  };
 
   render() {
     return (
@@ -25,7 +55,26 @@ class PageHeader extends Component {
           <h3 className="header__title">Medium Clone</h3>
         </Link>
         {this.state.token === '' ? (
-          <UserAuthentication  />
+          <div className="authentication">
+            <button onClick={this.triggerModal} name="Sign Up" className="authentication__button">
+              Sign Up
+            </button>
+            <button onClick={this.triggerModal} name="Log In" className="authentication__button">
+              Log In
+            </button>
+            <Modal
+              isOpen={this.state.modalIsOpen}
+              contentLabel={this.state.modalContent}
+              onRequestClose={() => this.setState(() => ({ modalIsOpen: false }))}
+            >
+              <a onClick={this.closeModal}>&times;</a>
+              {this.state.modalContent === 'Sign Up' ? (
+                <SignUpForm />
+              ) : (
+                  <LogInForm onSubmit={this.onSubmit} error={this.props.error} />
+                )}
+            </Modal>
+          </div>
         ) : (
             <Link to={`/profile/${this.state.username}`} >
               Welcome, {this.state.username}!
@@ -39,11 +88,13 @@ class PageHeader extends Component {
 }
 
 const mapStateToProps = state => ({
-  message: state.messages.message
+  message: state.messages.message,
+  error: state.errors.loginError
 });
 
 const mapDispatchToProps = dispatch => ({
-  setMessage: (message) => dispatch(setMessage(message))
+  setMessage: (message) => dispatch(setMessage(message)),
+  startLogIn: userCredentials => dispatch(startLogIn(userCredentials))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(PageHeader);
