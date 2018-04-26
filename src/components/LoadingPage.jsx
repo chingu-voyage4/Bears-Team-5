62aa91308cb9c74e069922e667a9f85fbab560d8
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-04-26 13:36:02 +0200

    Create and apply classes to LoadingPage

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const LoadingPage = () => (
-  <div className="container">
-    <p>Loading ...</p>
-    <i class="fas fa-cog fa-pulse fa-5x"></i>
+  <div className="container loading">
+    <p className="loading__title">Loading ...</p>
+    <i class="fas fa-cog fa-spin fa-5x"></i>
   </div>
 )
 
