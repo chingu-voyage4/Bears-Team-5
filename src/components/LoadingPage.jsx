742344bac3d09b4cb02f418add261dc8ca0da966
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-04-26 13:30:16 +0200

    Create LoadingPage component

---
+++ src/components/LoadingPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LoadingPage = () => (
+  <div className="container">
+    <p>Loading ...</p>
+    <i class="fas fa-cog fa-pulse fa-5x"></i>
+  </div>
+)
+
+export default LoadingPage;
