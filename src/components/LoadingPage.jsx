05527805184c0ee6b6435fffcd689a092a9a8a71
Jethro W. Fredericks <fredericks22.jethro@gmail.com>
2018-05-03 19:54:06 +0200

    Fix incorrectly named prop

--- src/components/LoadingPage.jsx
+++ src/components/LoadingPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LoadingPage = () => (
   <div className="container loading">
     <p className="loading__title">Loading ...</p>
-    <i class="fas fa-cog fa-spin fa-5x"></i>
+    <i className="fas fa-cog fa-spin fa-5x"></i>
   </div>
 )
 
